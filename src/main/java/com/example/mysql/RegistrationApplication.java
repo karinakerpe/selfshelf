578ada66188f719fc1e6558bd7d4420b3ceda7e3
fearofdrawing <55512124+fearofdrawing@users.noreply.github.com>
2021-08-11 19:16:53 +0300

    Update RegistrationApplication.java

--- src/main/java/com/example/mysql/RegistrationApplication.java
+++ src/main/java/com/example/mysql/RegistrationApplication.java
@@ -1,5 +1,8 @@
 package com.example.mysql;
 
+import com.example.mysql.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,10 +11,30 @@ import org.springframework.context.annotation.Configuration;
 @SpringBootApplication
 @Configuration
 @EnableAutoConfiguration
-public class RegistrationApplication {
+public class RegistrationApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(RegistrationApplication.class, args);
+	}
+
+	@Autowired
+	private BookRepository bookRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		/*
+		 * Book book1 = new Book("Alice's Adventures in Wonderland", "Lewis Carroll",
+		 * 1998, 200); bookRepository.save(book1);
+		 *
+		 * Book book2 = new
+		 * Book("The Emperor's New Mind: Concerning Computers, Minds and The Laws of Physics"
+		 * , "Roger Penrose", 1989, 480); bookRepository.save(book2);
+		 *
+		 * Book book3 = new Book("Diary of a Genius", "Salvador Dali", 2017, 160);
+		 * bookRepository.save(book3);
+		 */
 	}
 
 }
