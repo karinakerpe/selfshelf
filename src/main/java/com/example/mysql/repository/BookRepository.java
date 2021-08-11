006918a01e5fd0b634fa7f273987a3d450598868
fearofdrawing <55512124+fearofdrawing@users.noreply.github.com>
2021-08-11 19:14:26 +0300

    Update BookRepository.java

--- src/main/java/com/example/mysql/repository/BookRepository.java
+++ src/main/java/com/example/mysql/repository/BookRepository.java
@@ -7,14 +7,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
-@Repository
-public interface BookRepository extends JpaRepository <Book,Long> {
-
-    List<Book> findAllByTitle(String title);
-    List<Book> findAllByIdBetween(Long startId, Long endId);
-    @Query(value = "select b from Book  b where b.title=:title AND b.author=:author")
-    List<Book> findByTitleAndAuthor(@Param("title") String title, @Param("author") String firstName);
-
-public List<Book> findAll();
+public interface BookRepository extends JpaRepository<Book, Long> {
 
 }
+
