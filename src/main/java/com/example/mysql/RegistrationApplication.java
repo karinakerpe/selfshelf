bd2408aba7d641ab8ef14f6811e5bee034065706
svetlanatretjakova <svetlana.tretjakova9@gmail.com>
2021-08-19 01:28:21 +0300

    HTML index updated

--- src/main/java/com/example/mysql/RegistrationApplication.java
+++ src/main/java/com/example/mysql/RegistrationApplication.java
@@ -30,7 +30,7 @@ public class RegistrationApplication implements CommandLineRunner {
 
 		 Book book1 = new Book("Alice's Adventures in Wonderland", "Lewis Carroll", 1998, 200, "019283374X", BookStatus.AVAILABLE);
 		 bookRepository.save(book1);
-		 Book book2 = new Book("The Emperor's New Mind: Concerning Computers, Minds and The Laws of Physics", "Roger Penrose", 1989, 480, "0192861980", BookStatus.AVAILABLE);
+		 Book book2 = new Book("The Emperor's New Mind", "Roger Penrose", 1989, 480, "0192861980", BookStatus.AVAILABLE);
 		 bookRepository.save(book2);
 		 Book book4 = new Book("Labyrinths", "Jorge Luis Borges", 2007, 240, "0811216993", BookStatus.AVAILABLE);
 		 bookRepository.save(book4);
